ea77d9caa2c390087cf1cbdec25af774edfae936
ziaa <putriaprilia7415gmail.com>
2026-06-17 15:47:06 +0700

    [Tahap 4] Membuat class TiketIMAX

---
+++ classes/TiketIMAX.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once "Tiket.php";
+
+class TiketIMAX extends Tiket
+{
+    private $kacamata3dId;
+    private $efekGerakFitur;
+
+    public function __construct(
+        $id_tiket,
+        $nama_film,
+        $jadwal_tayang,
+        $jumlah_kursi,
+        $hargaDasarTiket,
+        $kacamata3dId,
+        $efekGerakFitur
+    ){
+        parent::__construct(
+            $id_tiket,
+            $nama_film,
+            $jadwal_tayang,
+            $jumlah_kursi,
+            $hargaDasarTiket
+        );
+
+        $this->kacamata3dId = $kacamata3dId;
+        $this->efekGerakFitur = $efekGerakFitur;
+    }
